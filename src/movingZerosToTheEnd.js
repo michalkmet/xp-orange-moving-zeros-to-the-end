f69a5228b331e696c44560d413c5c2a332f228b6
Michal Kmet <Michal.Kmet1@ibm.com>
2024-02-12 12:58:19 +0100

    refactor: complexity

--- src/movingZerosToTheEnd.js
+++ src/movingZerosToTheEnd.js
@@ -1,27 +1,22 @@
 function movingZerosToTheEnd(inputArr) {
   let resultArr = [];
-  if (!inputArr.includes(0)) {
-    return inputArr;
-  }
-
   let zerosCount = 0;
   for (let element of inputArr) {
-    console.log('element: ', element);
-    console.log('element != 0: ', element != 0);
     if (element != 0 || element === false) {
       resultArr.push(element);
     } else {
       zerosCount++;
     }
   }
-  console.log('zerosCount: ', zerosCount);
-  console.log('resultArr: ', resultArr);
+  return addZeros(zerosCount, resultArr);
+}
+
+function addZeros(zerosCount, resultArr) {
   if (zerosCount > 0) {
     for (let j = 0; j < zerosCount; j++) {
       resultArr.push(0);
     }
   }
-  console.log('resultArr2: ', resultArr);
   return resultArr;
 }
 
